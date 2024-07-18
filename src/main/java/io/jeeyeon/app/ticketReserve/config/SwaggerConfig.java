41439edbb66cbaa49b78a29603fe52f7d4a23db0
jy-cheon <emily.blunt983@gmail.com>
2024-07-19 04:23:48 +0900

    스웨거설정,인터셉터,필터

--- src/main/java/io/jeeyeon/app/ticketReserve/config/SwaggerConfig.java
+++ src/main/java/io/jeeyeon/app/ticketReserve/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
     public GroupedOpenApi concertApi() {
         return GroupedOpenApi.builder()
                 .group("concertApi")
-                .pathsToMatch("/concerts/**","/payment/**","/token/**","/users/**")
+                .pathsToMatch("/api/**")
                 .build();
     }
 
